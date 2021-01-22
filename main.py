654b50c621855446773d1b87d95d72739b946291
trl242 <royaurallockhart@gmail.com>
2021-01-22 14:23:28 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from tkinter import *
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+
+window = Tk()
+window.title("Flash Cards")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_img)
+canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+
+
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0)
+
+
+
+
+
+
+
+
+window.mainloop()
